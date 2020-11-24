4887f4237824aecb22742dddc94f076743010a28
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2020-11-23 21:01:42 -0300

    Criada lógica de login

---
+++ MusicApp/src/app/services/persisted-state.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersistedStateService {
+
+  constructor() { }
+
+  public LOGGED_IN = "AUTHENTICATED_USER";
+
+  public set (key:string, data:any){
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+
+  public get(key:string) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  public remove(key:string) {
+    localStorage.removeItem(key);
+  }
+
+  public exists(key:string) {
+    return !!localStorage.getItem(key);
+  }
+}
