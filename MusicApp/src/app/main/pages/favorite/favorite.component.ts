5309478f70d3878038c6979b0ab46a5db60e2f95
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2020-11-24 10:03:50 -0300

    Implementada remoção de música favorita

--- MusicApp/src/app/main/pages/favorite/favorite.component.ts
+++ MusicApp/src/app/main/pages/favorite/favorite.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import User from 'app/model/user';
 import { PersistedStateService } from 'app/services/persisted-state.service';
+import { UserService } from 'app/services/user.service';
 import Swal from "sweetalert2";
 
 @Component({
@@ -23,9 +24,11 @@ export class FavoriteComponent implements OnInit {
     ];
     dataSource: any[];
 
-    constructor(private persistedState:PersistedStateService, private router:Router) {
-
-    }
+    constructor(
+        private persistedState:PersistedStateService, 
+        private router:Router,
+        private userService:UserService
+    ) {}
 
     ngOnInit() {
         this.user = this.persistedState.get(this.persistedState.LOGGED_IN);
@@ -34,5 +37,30 @@ export class FavoriteComponent implements OnInit {
 
     goToDetail(albumId) {
         this.router.navigate(["music", albumId]);
+    }
+
+    removeFromFavorite(musicId) {
+        this.userService
+            .removeFromFavorite(this.user.id, musicId) // Remove da listagem de favoritos do usuário (banco de dados)
+            .subscribe(() => {
+                // Remove da listagem de favoritos do usuário (front end; dados persistidos)
+                this.user.favoriteMusics = this.user.favoriteMusics.filter(x => x.musicId != musicId);
+
+                // Atualiza os dados em cache do usuário
+                this.persistedState.set(
+                    this.persistedState.LOGGED_IN,
+                    this.user
+                );
+
+                // Atualiza os dados exibidos ao usuário
+                this.dataSource = this.user.favoriteMusics as any;
+
+                // Exibe pop up de confirmação
+                Swal.fire(
+                    "Sucesso!",
+                    "Música removida dos favoritos",
+                    "success"
+                );
+        });
     }
 }
--- MusicApp/src/app/services/user.service.ts
+++ MusicApp/src/app/services/user.service.ts
@@ -15,4 +15,13 @@ export class UserService {
   public authenticate(payload:SignIn):Observable<User> {
     return this.http.post<User>(`${environment.baseUrl}/User/authenticate`, payload);
   }
+
+  /**
+   * removeFromFavorite
+   * A API não retorna dados nesse caso, então o retorno do método foi
+   * declarado como any (qualquer coisa)
+   */
+  public removeFromFavorite(id:string, musicId:string):Observable<any>  {
+    return this.http.delete(`${environment.baseUrl}/User/${id}/favorite-music/${musicId}`);
+  }
 }
